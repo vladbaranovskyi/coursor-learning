b9e865b681e5546634f53a0a2692a9b37d0c3d41
John Doe <vladimir.bar16@gmail.com>
2021-02-22 14:27:52 +0200

    Add HW 2

---
+++ 01-homework/main.js
@@ -0,0 +1,29 @@
+let sum = 0;
+let N;
+let M;
+
+do {
+  N = +prompt ("Введите чило N");
+} while (!Number.isInteger(N) || isNaN(N))
+console.log(N);
+
+do {
+  M = +prompt ("Введите чило M");
+} while (!Number.isInteger(M) || isNaN(M))
+console.log(M);
+
+let isParedCount = confirm ("Пропускать парные числа при сложении?");
+
+for (var i = N; i <= M; i++)
+  {
+    if (isParedCount == true) {
+      if (i % 2 !== 0) {
+        sum += i;
+      }
+    }
+      else {
+        sum += i;
+    }
+  }
+
+document.writeln(`Сумма чисел равна: ${sum}`)
