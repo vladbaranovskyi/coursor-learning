d279f7d1c82f1d0328facd7588eae01aeda10ce1
John Doe <vladimir.bar16@gmail.com>
2021-02-23 16:27:48 +0200

    добавлена проверка больше ли число n чем m

--- 01-homework/main.js
+++ 01-homework/main.js
@@ -4,17 +4,21 @@ let M;
 
 do {
   N = +prompt ("Введите чило N");
-} while (!Number.isInteger(N) || isNaN(N))
+} while (!Number.isInteger(N) || isNaN(N));
 console.log(N);
 
 do {
   M = +prompt ("Введите чило M");
-} while (!Number.isInteger(M) || isNaN(M))
+} while (!Number.isInteger(M) || isNaN(M));
 console.log(M);
+
+while (M < N) {
+  M = +prompt ("M должно быть больше N, введите число M");
+}
 
 let isParedCount = confirm ("Пропускать парные числа?");
 
-for (let i = N; i <= M; i++)
+for (var i = N; i <= M; i++)
   {
     if (isParedCount == true) {
       if (i % 2 !== 0) {
