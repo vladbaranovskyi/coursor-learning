dc120a2df7cf1772202731716bf0a0b313bc7cb5
John Doe <vladimir.bar16@gmail.com>
2021-03-13 14:37:55 +0200

    Add NW #7

--- 06-homework/main.js
+++ 06-homework/main.js
@@ -10,3 +10,37 @@ function getMyTaxes(salary) {
 }
 
 console.log ('Налог в Латвии при ЗП 2000: ', getMyTaxes.call(latvia, 2000));
+
+// Среднее количество налогов в каждой стране
+
+function getMiddleTaxes() {
+  return this.tax * this.middleSalary;
+}
+
+console.log ('Среднее количество налогов в Латвии: ', getMiddleTaxes.call(latvia));
+console.log ('Среднее количество налогов в Украине: ', getMiddleTaxes.call(ukraine));
+console.log ('Среднее количество налогов в Литве: ', getMiddleTaxes.call(litva));
+
+// Сколько всего налогов платят в каждой стране
+
+function getTotalTaxes() {
+  return this.tax * this.middleSalary * this.vacancies;
+}
+
+console.log ('Общее количество налогов в Латвии: ', getTotalTaxes.call(latvia));
+console.log ('Общее количество налогов в Украине: ', getTotalTaxes.call(ukraine));
+console.log ('Общее количество налогов в Литве: ', getTotalTaxes.call(litva));
+
+// Пересчет ЗП с интервалом в 10 секунд
+
+function getMySalary(country) {
+  setInterval(() => {
+    const mySalary = {};
+    mySalary.salary = Math.floor(Math.random() * (2000 - 1500)) + 1500;
+    mySalary.taxes = Math.floor(mySalary.salary * country.tax);
+    mySalary.profit = Math.floor(mySalary.salary - mySalary.taxes);
+    console.log('ЗП в Литве:', mySalary);
+  }, 10000);
+}
+
+getMySalary(litva);
