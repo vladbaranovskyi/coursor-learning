3c524d92105e615b2f40007d6a72ee0bd7910b60
John Doe <vladimir.bar16@gmail.com>
2021-03-15 01:51:21 +0200

    Add part of HW 8

--- 07-homework/main.js
+++ 07-homework/main.js
@@ -1,19 +1,41 @@
-const student = {
-  university: `Вища Школа Психотерапії м. Одеса`,
-  course: 1,
-  fullName: `Остап Родоманський Бендер`
-};
-
-
-
-// Сколько налогов нужно заплатить в определенной стране
-
 class Student {
-  constructor(studentInfo) {
-     this.university = university,
-     this.course - course,
-     this.fullName = fullName,
-   }
+
+  constructor(option) {
+    this.university = option.university
+    this.course = option.course
+    this.fullName = option.fullName
+    this.marks = option.marks
+  }
+
+  get getInfo () {
+    return `Студент ${this.course}-го курсу ${this.university}, ${this.fullName}`;
+  }
+
+  get getMarks () {
+    return this.marks;
+  }
+
+  set setMarks (mark) {
+    if (mark === 0 || mark > 5) {
+      return this.marks
+    }
+    else {
+      this.marks.push(mark)
+    }
+  }
+
+  
 }
 
-console.log (Student);
+const student = new Student ({
+  university: `Вищої Школи Психотерапії м. Одеса`,
+  course: 1,
+  fullName: `Остап Бендер`,
+  marks: [5, 4, 4, 5]
+});
+
+
+console.log(student.getInfo);
+console.log(student.getMarks);
+student.setMarks = 5
+console.log(student.marks);
