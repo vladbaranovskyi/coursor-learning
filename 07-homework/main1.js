8b772a62d7bbbb835f106624e1692846357f5002
unknown <vladimir.bar16@gmail.com>
2021-03-25 00:08:02 +0300

    123

--- 07-homework/main1.js
+++ 07-homework/main1.js
@@ -1,41 +1,32 @@
 class Student {
-    constructor(university, course, fullName, studentMarks = [5, 4, 4, 5], isStudent = false) {
+    constructor(university, course, fullName, studentMarks = [5, 4, 4, 5]) {
         this.university = university;
         this.course = course;
         this.fullName = fullName;
         this.studentMarks = studentMarks;
-        this.isStudent = isStudent;
-    }
 
+    }
     getInfo() {
         return `Студент ${this.course}го курсу університету ${this.university}, ${this.fullName}`;
     }
-
     get marks() {
-        if (!this.isStudent) {
-            return this.studentMarks
-        }
-
+        return this.studentMarks
     }
-
     set setMarks(mark) {
-        if (!this.isStudent) {
+        if(this.studentMarks != null || mark > 5) {
             return this.studentMarks.push(mark);
         }
     }
-
     getAverageMark() {
-        if(!this.isStudent) {
+        if(this.studentMarks != null) {
             return this.studentMarks.reduce(getAverage, 0) / this.studentMarks.length;
         }
     }
-
     dismiss() {
-        this.isStudent = true;
-        console.log(`${this.fullName} був відрахований`)
+        this.studentMarks = null;
     }
     recover() {
-            this.isStudent = false;
+        this.studentMarks = [];
     }
 }
 
@@ -46,7 +37,7 @@ function getAverage(sum, current) {
 const student = new Student(
     `Вищої Школи Психотерапії м. Одеса`,
     1,
-    'Остап Бендер'
+    'Остап Бендер',
 );
 
 console.log(student.getInfo());
@@ -55,19 +46,27 @@ student.setMarks = 5;
 console.log(student.marks);
 console.log(student.getAverageMark());
 student.dismiss();
+student.setMarks = 5;
+console.log(student.marks);
 student.recover();
+student.setMarks = 5
+student.setMarks = 4
+student.setMarks = 4
+student.setMarks = 5
 console.log("Оцінки студента після відновлення:", student.marks);
 
 //Advanced
 
 class BudgetStudent extends Student {
-    constructor(university, course, fullName, studentMarks, isStudent) {
-        super(university, course, fullName, studentMarks, isStudent);
+    constructor(university, course, fullName, studentMarks) {
+        super(university, course, fullName, studentMarks);
         this.getScholarship = function getScholarship() {
-            if(this.getAverageMark() >= 4 && this.isStudent === false) {
+            if(this.getAverageMark() >= 4) {
                 console.log(`${this.fullName} отрмав 1400$ стипендії`)
+            } else if(this.studentMarks === null) {
+                console.log(`${this.fullName} покинув навчання, запустив Facebook та бiльше не отримує стипендію`);
             } else {
-                console.log(`${this.fullName} не отримав стипендію`);
+                console.log(`${this.fullName} не отрмав стипендію`);
             }
         }
         setInterval(() => this.getScholarship(), 30000);
@@ -84,4 +83,8 @@ budgetStudent.getScholarship();
 budgetStudent.dismiss();
 budgetStudent.getScholarship();
 budgetStudent.recover();
+student.setMarks = 5
+student.setMarks = 4
+student.setMarks = 4
+student.setMarks = 5
 budgetStudent.getScholarship();
--- 08-homework/main.js
+++ 08-homework/main.js
@@ -1,41 +1,31 @@
 class Student {
-    constructor(university, course, fullName, studentMarks = [5, 4, 4, 5], isStudent = false) {
+    constructor(university, course, fullName, studentMarks = [5, 4, 4, 5]) {
         this.university = university;
         this.course = course;
         this.fullName = fullName;
         this.studentMarks = studentMarks;
-        this.isStudent = isStudent;
     }
-
     getInfo() {
         return `Студент ${this.course}го курсу університету ${this.university}, ${this.fullName}`;
     }
-
     get marks() {
-        if (!this.isStudent) {
-            return this.studentMarks
-        }
-
+        return this.studentMarks
     }
-
     set setMarks(mark) {
-        if (!this.isStudent) {
+        if(this.studentMarks != null || mark > 5) {
             return this.studentMarks.push(mark);
         }
     }
-
     getAverageMark() {
-        if(!this.isStudent) {
+        if(this.studentMarks != null) {
             return this.studentMarks.reduce(getAverage, 0) / this.studentMarks.length;
         }
     }
-
     dismiss() {
-        this.isStudent = true;
-        console.log(`${this.fullName} був відрахований`)
+        this.studentMarks = null;
     }
     recover() {
-            this.isStudent = false;
+        this.studentMarks = [];
     }
 }
 
@@ -55,19 +45,27 @@ student.setMarks = 5;
 console.log(student.marks);
 console.log(student.getAverageMark());
 student.dismiss();
+student.setMarks = 5;
+console.log(student.marks);
 student.recover();
+student.setMarks = 5
+student.setMarks = 4
+student.setMarks = 4
+student.setMarks = 5
 console.log("Оцінки студента після відновлення:", student.marks);
 
 //Advanced
 
 class BudgetStudent extends Student {
-    constructor(university, course, fullName, studentMarks, isStudent) {
-        super(university, course, fullName, studentMarks, isStudent);
+    constructor(university, course, fullName, studentMarks) {
+        super(university, course, fullName, studentMarks);
         this.getScholarship = function getScholarship() {
-            if(this.getAverageMark() >= 4 && this.isStudent === false) {
+            if(this.getAverageMark() >= 4) {
                 console.log(`${this.fullName} отрмав 1400$ стипендії`)
+            } else if(this.studentMarks === null) {
+                console.log(`${this.fullName} покинув навчання, запустив Facebook та бiльше не отримує стипендію`);
             } else {
-                console.log(`${this.fullName} не отримав стипендію`);
+                console.log(`${this.fullName} не отрмав стипендію`);
             }
         }
         setInterval(() => this.getScholarship(), 30000);
@@ -84,4 +82,8 @@ budgetStudent.getScholarship();
 budgetStudent.dismiss();
 budgetStudent.getScholarship();
 budgetStudent.recover();
+student.setMarks = 5
+student.setMarks = 4
+student.setMarks = 4
+student.setMarks = 5
 budgetStudent.getScholarship();
